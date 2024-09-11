7904f326af06858ed1a89c2264e514c632b5c566
ianalexmac <iamacdougall@alaska.edu>
2024-09-11 15:57:27 -0800

    reorganize data directory structure

--- src/pipeline/main.py
+++ src/pipeline/main.py
@@ -9,8 +9,7 @@ from src.pipeline.api_geo import geo_save
 # pull plant locations, save as geojson
 xlsx_url_to_file(
     xlsx_url = 'https://raw.githubusercontent.com/acep-uaf/ak-energy-statistics-2011_2021/main/workbooks/Energy_Stats_Infrastructure_2021.xlsx', 
-    file = 'data/raw/Energy_Stats_Infrastructure_2021.xlsx'
-)
+    file = 'data/raw/Energy_Stats_Infrastructure_2021.xlsx')
 
 df = pd.read_excel(
         'data/raw/Energy_Stats_Infrastructure_2021.xlsx',
@@ -24,58 +23,47 @@ gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.Latitude), crs
 
 # save geodatabase to file as geojson
 geo_save(
-    geo_input = 'data/raw/Alaska_population_locations.gdb.zip',
-    geo_output = 'data/raw/alaska_population_locations.geojson'
-)
+    geo_input = 'data/source/Alaska_population_locations.gdb.zip',
+    geo_output = 'data/raw/alaska_population_locations.geojson')
 
 # save ak_coastline to file from DNR
 geo_save(
     geo_input = 'https://arcgis.dnr.alaska.gov/arcgis/rest/services/OpenData/Physical_AlaskaCoast/MapServer/1/query?outFields=*&where=1%3D1&f=geojson',
-    geo_output = 'data/raw/alaska_coastline.geojson'
-)
+    geo_output = 'data/raw/alaska_coastline.geojson')
 
 # save community locations from DCCED
 geo_save(
     geo_input = 'https://maps.commerce.alaska.gov/server/rest/services/Community_Related/Community_Locations_and_Boundaries/MapServer/0/query?outFields=*&where=1%3D1&f=geojson',
-    geo_output = 'data/raw/community_locations.geojson'
-)
+    geo_output = 'data/raw/community_locations.geojson')
 
 # save Electric North boundary (from .gitignored directory, downloaded from Google Drive) to file 
 geo_save(
-    geo_input = 'data/raw/Shapefile_export/EN_outline.shp',
-    geo_output = 'data/raw/en_outline.geojson'
-)
+    geo_input = 'data/source/Shapefile_export/EN_outline.shp',
+    geo_output = 'data/raw/en_outline.geojson')
 
 # save Electric North regional grids (from .gitignored directory, downloaded from Google Drive) to file
 geo_save(
-    geo_input = 'data/raw/Shapefile_export/EN_regionalGrids.shp',
-    geo_output = 'data/raw/en_regional_grids.geojson'
-)
+    geo_input = 'data/source/Shapefile_export/EN_regionalGrids.shp',
+    geo_output = 'data/raw/en_regional_grids.geojson')
 
 # save Electric North regional grid capacity (from .gitignored directory, downloaded from Google Drive) to file
 geo_save(
-    geo_input = 'data/raw/Shapefile_export/EN_regionalGrids_capacity.shp',
-    geo_output = 'data/raw/en_regional_grids_capacity.geojson'
-)
+    geo_input = 'data/source/Shapefile_export/EN_regionalGrids_capacity.shp',
+    geo_output = 'data/raw/en_regional_grids_capacity.geojson')
 
 # save AETR generation data to file
 (pd.read_csv('https://raw.githubusercontent.com/acep-uaf/aetr-web-book-2024/main/data/final_data/generation.csv')
     .dropna(subset=['generation'])
     .query('generation >= 0')
-    .to_csv('data/derived/generation.csv')
-)
-
-
-
+    .to_csv('data/derived/generation.csv'))
 
 
 # run transmission line cleaning script
 from src.pipeline.clean_aea_transmission_lines import voltage_cleaning
 
 voltage_cleaning(
-    input = 'data/raw/Shapefile_export/AEA-Transmission_Lines-202200706/Alaska_Energy_Authority_Library.shp',
-    output = 'data/derived/transmission.geojson'
-)
+    input = 'data/source/Shapefile_export/AEA-Transmission_Lines-202200706/Alaska_Energy_Authority_Library.shp',
+    output = 'data/derived/transmission.geojson')
 
 
 
