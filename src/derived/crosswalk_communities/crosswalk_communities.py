5eff3efbe084b23c5b5faa25534d3ed4eac1c530
ianalexmac <iamacdougall@alaska.edu>
2024-09-17 14:19:55 -0800

    add successful yaml render

--- src/derived/crosswalk_communities/crosswalk_communities.py
+++ src/derived/crosswalk_communities/crosswalk_communities.py
@@ -1,44 +1,91 @@
 import yaml
 import pandas as pd
 import geopandas as gpd
+from fuzzywuzzy import process
 
 
 
-def make_yaml(df_path, column_name:str):
+def make_yaml(df_path, id_column: str, name_column: str, yaml_path):
     # read file
     df = gpd.read_file(df_path)
-    # select column of names
-    c = df[[column_name]]
-    # construct dictionary of names with empty alias
-    dict = c.to_dict()
-    yaml_string = yaml.dump(dict)
-    print(yaml_string, 'test.yaml')
-    # write to file as yaml
 
-df_path = 'data/raw/dcced_communities.geojson'
+    # # select column of names
+    # c = df[[id_column, name_column]]
+    # # make dictionary of names with empty alias
+    # dict = c.to_dict()
 
-make_yaml(df_path, 'CommunityName')
+    d = df.set_index(id_column)[name_column].to_dict()
 
+    # yaml.dump({k: {'name': v} for k, v in d.items()}, open(yaml_path, 'w'), default_flow_style=False)
+    yaml.dump({k: {'name': v, 'alias': None} for k, v in d.items()}, open(yaml_path, 'w'), default_flow_style=False)
 
 
+    # # write to file as yaml
+    # with open(yaml_path, 'w') as outfile:
+    #     yaml.dump(d, outfile, default_flow_style=False)
+
+df_path = 'data/raw/usgs_places.geojson'
+id_column = 'gaz_id'
+name_column = 'gaz_name'
+yaml_path = 'data/derived/crosswalk_communities.yaml'
+
+make_yaml(df_path, id_column, name_column, yaml_path)
 
 
 
 
 
-def add_alias(yaml_file):
-    # import yaml of aliases
-    with open(yaml_file) as f:
+
+
+
+
+
+
+def add_alias(df_path, name_column: str, yaml_path):
+    # import yaml of aliases as dictionary
+    with open(yaml_path) as f:
         aka = yaml.safe_load(f)
+    # import dataframe with uncleaned names
+    df = gpd.read_file(df_path)
+    # pull name column from uncleaned dataframe, rename, make dictionary
+    names = df[name_column].rename('name').to_dict()
+
+    # fuzzy match to dictionary of standardized names (for loop)
+    for community in names.values():
+        print(community)
+
+
+    for alias in aka.values():
+        print(alias)
+
+    # add matched names as aliases to dictionary of standardized names (for loop)
+    # new_aliases = ['Utqiagvik (formerly Barrow)', 'City of Utqiagvik']
+    # for community in aka['communities']:
+    #     community['aliases'].extend(new_aliases)
+
+
+    # write to file dictionary of standardized names
+    with open(yaml_path, 'w') as outfile:
+        yaml.dump(aka['communities'], outfile, default_flow_style=False)
     return(aka)
 
-add_alias('src/derived/crosswalk_communities/crosswalk.yaml')
+
+df_path = 'data/raw/usgs_places.geojson'
+name_column = 'gaz_name'
+yaml_path = 'data/derived/crosswalk_communities.yaml'
+
+add_alias(df_path, name_column, yaml_path)
 
 
-for community in aka['communities']: 
-    print(community)
 
-join = cap.merge(gen, on='town')
+def standardize_names(df, name_column: str, yaml_path: str, output_file: str):
+    # import yaml of aliases as dictionary
+    # import dataframe with uncleaned names
+    # loop through alias dictionary for matching aliases, overwrite
+    # save dataframe to file, different location
+
+
+
 
 
 
@@ -59,10 +106,14 @@ c = {'town': ['Barrow', 'City of Craig', 'Sitka Borough', 'Petersburg', 'Kake'],
     'capacity': [3, 4, 2, 3, 1]}
 cap = pd.DataFrame(data=c)
 
-
+join = cap.merge(gen, on='town')
 
 coast = gpd.read_file('data/derived/coastline.geojson')
 coast.explore()
+
+
+
+
 
 
 # Yaml target
