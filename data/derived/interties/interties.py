dd8b946a35b7097dc784f507a8439e1e4ee210d4
ianalexmac <iamacdougall@alaska.edu>
2024-09-26 10:15:38 -0800

    deprecate code

--- data/derived/interties/interties.py
+++ data/derived/interties/interties.py
@@ -4,4 +4,4 @@ lookup_interties = pd.read_csv('data/raw/es/data/es_lookup_interties.csv')
 
 interties = lookup_interties[lookup_interties['Current ID'] == True].drop(columns=['Communities Intertied'])
 
-interties.to_csv('data/derived/interties/interties.csv')
+interties.to_csv('data/derived/interties/interties.csv', index=False)
